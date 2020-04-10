9487081d461463ec134400420ea7d5168b82d238
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-04-10 19:38:03 +0200

    Fixing

--- bokscrapper.py
+++ bokscrapper.py
@@ -8,13 +8,18 @@ from bs4 import BeautifulSoup
 # Setting headers
 headers = requests.utils.default_headers()
 headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
-q = input("\033[0;35mWhat Are you looking for ? : \033[0m")
-url = "https://b-ok.cc/s/" + q
-req = requests.get(url, headers)
-soup = BeautifulSoup(req.content, 'html.parser')
-total = soup.find(class_="totalCounter")
-for nb in total.descendants:
-    nbx = nb.replace("(", "").replace(")", "")
+
+def search():
+    logo()
+    q = input("\033[0;35mWhat Are you looking for ? : \033[0m")
+    url = "https://b-ok.cc/s/" + q
+    req = Request(url)
+    req.add_header(headers[0],headers[1]) 
+    content = urlopen(req).read()
+    soup = BeautifulSoup(content, 'html.parser')
+    total = soup.find(class_="totalCounter")
+    for nb in total.descendants:
+        nbx = nb.replace("(", "").replace(")", "")
     print("\n\033[1;33mThere Are " + nbx + " Books about " + q + " :\033[0m\n")
 for tr in soup.find_all('td'):
     for td in tr.find_all('h3'):
